1ff2ae3353ac34c19b46c17f2e1eb109b175a36c
Erin  Rose <errose10@gmail.com>
2020-01-29 19:06:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,15 @@ attr_accessor :name, :artist
     @@all
   end
 
-  def new_by_filename
+  def new_by_filename(filename)
+    #artist - song_name - genre.mp3
+    #artist
+    #song_name
+    artist = filename.split(' - ')[0]
+    song_name = filename.split(' - ')[1]
+    new_song = Song.new(song_name)
+    new_song.artist = artist
+    
   end
 
 end
